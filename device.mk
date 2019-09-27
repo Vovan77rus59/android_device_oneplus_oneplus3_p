7a3370211b462b2ec9d4b6086a771933a5dfc4c8
Владимир <vova.perm@hotmail.com>
2019-09-27 13:26:15 +0500

    op3: enable framework-res pro

--- device.mk
+++ device.mk
@@ -30,7 +30,10 @@ $(call inherit-product, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
-
+    
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+    
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
